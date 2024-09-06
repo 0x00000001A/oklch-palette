ada278f753cea034d7cbc1bab832493b4b9fac62
Viktor Novikov <mydesireiscoma@outlook.com>
2024-09-06 14:27:09 +0700

    Comment-out the color space dropdown

--- src/components/Navbar/ColorSpaceDropdown/ColorSpaceDropdown.tsx
+++ src/components/Navbar/ColorSpaceDropdown/ColorSpaceDropdown.tsx
@@ -1,28 +1,30 @@
-import {FC, useCallback, useState} from 'react'
+import {FC} from 'react'
 
-import IconColorSpace from '../../../icons/IconColorSpace.tsx'
-import Dropdown from '../Dropdown/Dropdown.tsx'
-import {BaseOption} from '../Dropdown/types.ts'
+// import IconColorSpace from '../../../icons/IconColorSpace.tsx'
+// import Dropdown from '../Dropdown/Dropdown.tsx'
+// import {BaseOption} from '../Dropdown/types.ts'
 
-import {colorSpacesOptions} from './data.tsx'
+// import {colorSpacesOptions} from './data.tsx'
 
+// @todo to be implemented
 const ColorSpaceDropdown: FC = () => {
-  const [value, setValue] = useState<BaseOption>(colorSpacesOptions[0])
-
-  const handleChange = useCallback((option: BaseOption) => {
-    document.body.style.filter = option.raw ? option.raw : `url(#${option.value})`
-    setValue(option)
-  }, [])
-
-  return (
-    <Dropdown
-      icon={<IconColorSpace />}
-      optionLabelProp={'label'}
-      options={colorSpacesOptions}
-      value={value}
-      onChange={handleChange}
-    />
-  )
+  // const [value, setValue] = useState<BaseOption>(colorSpacesOptions[0])
+  //
+  // const handleChange = useCallback((option: BaseOption) => {
+  //   document.body.style.filter = option.raw ? option.raw : `url(#${option.value})`
+  //   setValue(option)
+  // }, [])
+  //
+  // return (
+  //   <Dropdown
+  //     icon={<IconColorSpace />}
+  //     optionLabelProp={'label'}
+  //     options={colorSpacesOptions}
+  //     value={value}
+  //     onChange={handleChange}
+  //   />
+  // )
+  return null
 }
 
 export default ColorSpaceDropdown

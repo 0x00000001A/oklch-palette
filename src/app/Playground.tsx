9030134f39f2793f06c8bf23caed9ef7eed359d2
Viktor Novikov <mydesireiscoma@outlook.com>
2024-09-06 14:28:41 +0700

    Comment-out the playground component

--- src/app/Playground.tsx
+++ src/app/Playground.tsx
@@ -1,29 +1,11 @@
 import {FC} from 'react'
 
-import {Button} from '../components/Button'
-import IconGithub from '../icons/IconGithub.tsx'
-
 export type PlaygroundProps = {
   // props
 }
 
 const Playground: FC<PlaygroundProps> = () => {
-  return (
-    <div>
-      <div>
-        <Button>
-          <IconGithub />
-          Button text
-        </Button>
-      </div>
-      <Button>Text only</Button>
-      <div>
-        <Button>
-          <IconGithub />
-        </Button>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Playground
